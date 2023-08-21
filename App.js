cfcb4ffe8ebfe5bed0df36d1b7454625bc146f8a
playbench-dev <dev@playbench.co.kr>
2023-08-21 13:15:19 +0900

    merge later

--- App.js
+++ App.js
@@ -7,94 +7,16 @@
  */
 
 import React from 'react';
-<<<<<<< HEAD
-import {StatusBar, Platform, Alert} from 'react-native';
-import {SafeAreaView, SafeAreaProvider, useSafeAreaInsets} from 'react-native-safe-area-context';
-import {NavigationContainer} from '@react-navigation/native';
-import Stack from './src/Common/Stack';
-import Users from './src/Common/User';
-import {navigationRef} from './src/Common/RootNavigation'
-=======
 import { StatusBar, Platform, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Stack from './src/Common/Stack';
 import Users from './src/Common/User';
 import { navigationRef } from './src/Common/RootNavigation'
->>>>>>> mw
 import { View } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
 import PushNotification from 'react-native-push-notification';
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
 import Home from './src/Home'
-<<<<<<< HEAD
-import Orientation from 'react-native-orientation';
-
-const TAG = "App";
-
-const CustomStatusBare = ({
-  backgroundColor,
-  barStyle = "dark-content",
-}) => {
-  const insets = useSafeAreaInsets();
-
-  return(
-    <View style = {{height : insets.top, backgroundColor}}>
-      <StatusBar animated={true} backgroundColor = {backgroundColor} barStyle = {barStyle}></StatusBar>
-    </View>
-  )
-}
-
-class App extends React.Component{
-  
-  state = {
-    iosPush : false,
-  }
-
-  async componentDidMount(){
-    console.log(TAG, messaging().hasPermission());
-    // Orientation.addOrientationListener(() => Orientation.lockToLandscapeLeft());
-    this._Notification();
-
-  }
-
-  _orientationDidChange = (orientation) => {
-    Orientation.lockToLandscapeLeft();
-    if (orientation === 'LANDSCAPE') {
-      // do something with landscape layout
-      console.log(TAG,'land left');
-      Orientation.lockToLandscapeLeft();
-    }
-  }
-
-  componentWillUnmount() {
-    // Orientation.removeOrientationListener(this._orientationDidChange);
-    this.messageListener();
-  }
-
-  _Notification(){
-    this.messageListener = messaging().onMessage(async remoteMessage => {
-      PushNotification.configure({
-        onNotification: function (notification) {
-            // const channerId = notification.data.android_channel_id;
-            console.log("NOTIFICATION:", ""+JSON.stringify(notification));
-            console.log("channel : " + JSON.stringify(remoteMessage));
-            if (notification.foreground && notification.userInteraction) {
-              if(remoteMessage.data.android_channel_id == "medicine"){
-                  navigationRef.current.navigate('MedicineCalendar',{orderNo : notification.no, medicineUpdate : Home._MedicineUpdate})
-              }else if(remoteMessage.data.android_channel_id == "embryo"){
-                navigationRef.current.navigate('CellDevelop',{orderNo : notification.no})
-              }else if(remoteMessage.data.android_channel_id == "home"){
-                navigationRef.current.navigate('Home',{orderNo : notification.no})
-              }else{
-                navigationRef.current.navigate('Home',{orderNo : notification.no})
-              }
-            }
-        }
-    });
-
-      PushNotification.localNotification({
-        channelId : remoteMessage.data.android_channel_id,
-=======
 import Toast from 'react-native-toast-message';
 
 const TAG = "App";
@@ -141,31 +63,11 @@ export default class App extends React.Component {
 
       PushNotification.localNotification({
         channelId: remoteMessage.data.android_channel_id,
->>>>>>> mw
         vibrate: true,
         vibration: 300,
         priority: 'high',
         visibility: 'public',
         importance: 'hight',
-<<<<<<< HEAD
-        title : remoteMessage.data.title,
-        message : remoteMessage.data.body, // (required)
-        playSound: false,
-        autoCancel : true,
-        smallIcon : 'ic_logo',
-        largeIcon : remoteMessage.data.image,
-        no : remoteMessage.data.no,
-        picture : remoteMessage.data.image,
-        largeIconUrl : remoteMessage.data.image
-      });
-
-      console.log("App",remoteMessage.data.image)
-    });
-    
-    //background
-    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-      console.log("App : ",'remote : ' + JSON.stringify(remoteMessage));
-=======
         title: remoteMessage.data.title,
         message: remoteMessage.data.body, // (required)
         playSound: false,
@@ -184,7 +86,6 @@ export default class App extends React.Component {
     messaging().setBackgroundMessageHandler(async (remoteMessage) => {
       console.log("App : ", 'remote : ' + JSON.stringify(remoteMessage));
 
->>>>>>> mw
       //  여기에 로직을 작성한다.
       //  remoteMessage.data로 메세지에 접근가능
       //  remoteMessage.from 으로 topic name 또는 message identifier
@@ -194,10 +95,6 @@ export default class App extends React.Component {
     });
 
     // messaging().getToken().then(token => AsyncStorage.setItem('token',token));
-<<<<<<< HEAD
-=======
-
->>>>>>> mw
     messaging().getToken().then(token => Users.token = token);
 
     console.log('token : ' + Users.token);
@@ -208,17 +105,10 @@ export default class App extends React.Component {
         "App onNotificationOpenedApp : ",
         remoteMessage.data
       );
-<<<<<<< HEAD
-      navigationRef.current.navigate('Splash',{orderNo : remoteMessage.data.no, channelId : remoteMessage.data.android_channel_id})
-      // navigation.navigate(remoteMessage.data.type);
-    });
-    
-=======
       navigationRef.current.navigate('Splash', { orderNo: remoteMessage.data.no, channelId: remoteMessage.data.android_channel_id })
       // navigation.navigate(remoteMessage.data.type);
     });
 
->>>>>>> mw
     //android background open app
     messaging()
       .getInitialNotification()
@@ -226,26 +116,6 @@ export default class App extends React.Component {
         if (remoteMessage) {
           console.log(
             "App getInitialNotification : ",
-<<<<<<< HEAD
-            remoteMessage.data 
-          );
-          console.log(TAG,"id : " + remoteMessage.data.android_channel_id  + " no : " + remoteMessage.data.no);
-          navigationRef.current.navigate('Splash',{orderNo : remoteMessage.data.no, channelId : remoteMessage.data.android_channel_id})
-        }
-      });
-    }
-
-  render(){
-    return (
-      <NavigationContainer ref = {navigationRef}>
-      <Stack/>
-    </NavigationContainer>
-    );
-  }
-}
-
-export default App;
-=======
             remoteMessage.data
           );
           console.log(TAG, "id : " + remoteMessage.data.android_channel_id + " no : " + remoteMessage.data.no);
@@ -264,4 +134,3 @@ export default App;
     );
   }
 }
->>>>>>> mw
